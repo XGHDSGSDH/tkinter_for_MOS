279498700eef71f08c7395589d6f8ab2a0b28d6c
duanwb <yuwenxuedai@163.com>
2023-08-29 14:37:00 +0800

    class Dataloader

---
+++ dataloader.py
@@ -0,0 +1,30 @@
+class Dataloader:
+    def __init__(self,*,dir):
+        self.list=[]
+        if dir:
+            import os
+            self.list = os.listdir(dir)
+            self.dir=dir
+    
+    def mess_up(self):
+        import random
+        self.list = random.shuffle(self.list)
+    
+    def save(self,*,file_pth):
+        with open(file_pth,'w') as outfile:
+            outfile.write(self.list)
+
+    def load(self,*,file_pth):
+        import os
+        if os.path.isfile(file_pth):
+            with open(file_pth) as readfile:
+                self.list=eval(readfile)
+            return True
+        else:
+            return False
+        
+    def __len__(self):
+        return len(self.list)
+
+    def __getitem__(self,idx):
+        return self.list[idx]
